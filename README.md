c4859dd6e53d90f0e5c9ed639b3324a84f76b5cb
amsterdan <amsterdan@163.com>
2021-11-01 20:12:51 +0800

    调整代码

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # auth
-auth client
+
+### 引入代码包
+
+```
+composer require amsterdan5\auth;
+```
+
+### 使用方法
+```
+$secret = 'uwwb0EfVj7Ox17T6YIC8GaMqqmHxNEstSD8cWIr3';
+$session = new Amsterdan\Auth\Session($secret);
+
+$en = $session->setUid(1)->setUserName('Job')->encrypt();
+var_dump($en);
+
+$de = $session->decrypt($en);
+var_dump($de);
+```
--- src/Amsterdan/Auth/Session.php
+++ src/Amsterdan/Auth/Session.php
@@ -11,7 +11,7 @@ class Session
     ];
 
     // 解密后的内容
-    private $decrypt = [];
+    private $options_data = [];
 
     // 密钥
     private $secret;
@@ -21,51 +21,100 @@ class Session
         $this->secret = $secret;
     }
 
+    /**
+     * 加密方法
+     */
+    public function encrypt()
+    {
+        $session = str_replace('-', '', $this->makeSession());
+        $sessionId = $session . $this->formatOptions();
+        return Aes::encrypt($sessionId, $this->secret);
+    }
+
+    /**
+     * 解密方法
+     *
+     * @param string $str 密文
+     */
+    public function decrypt(string $str)
+    {
+        if (empty($str)) {
+            return false;
+        }
+
+        $session = str_replace(' ', '+', rawurldecode($str));
+        $str = Aes::decrypt($session, $this->secret);
+        if (!$str) {
+            return false;
+        }
+
+        $options = substr($str, 32);
+        return $this->getOptions($options);
+    }
+
+    /**
+     * 设置用户id
+     */
     public function setUid($uid)
     {
         $this->options['uid'] = $uid;
         return $this;
     }
 
+    /**
+     * 设置用户名
+     */
     public function setUsername($name)
     {
         $this->options['username'] = $name;
         return $this;
     }
 
+    /**
+     * 设置域名
+     */
     public function setDomain($domain)
     {
         $this->options['domain'] = $domain;
         return $this;
     }
 
-    public function setSecret($secret)
-    {
-        $this->secret = $secret;
-        return $this;
-    }
-
+    /**
+     * 获取用户id
+     */
     public function getUid()
     {
         return $this->options['uid'];
     }
 
+    /**
+     * 获取用户名
+     */
     public function getUserName()
     {
         return $this->options['username'];
     }
 
+    /**
+     * 获取域名
+     */
     public function getDomain()
     {
         return $this->options['domain'];
     }
 
-    private function genOptions()
+    /**
+     * 返回解密后的内容
+     */
+    private function formatOptions()
     {
         return implode(',', $this->options);
     }
 
-    private function session($session)
+    /**
+     * 格式化 session
+     */
+    private function formatSession($session)
     {
         $parts = [];
         $pattern = [
@@ -83,37 +132,27 @@ class Session
         return implode('-', $parts);
     }
 
-    public function makeSession()
+    /**
+     * 生成 session
+     *
+     * @param bool $format  格式化
+     */
+    public function makeSession(bool $format = false)
     {
-        return $this->session(md5(time() . uniqid()));
+        $session = md5(time() . uniqid());
+        return $format ? $this->formatSession($session) : $session;
     }
 
-    public function genSessionId($session)
+    /**
+     * 获取设置的内容
+     */
+    private function getOptions($options)
     {
-        return $session . $this->genOptions();
-    }
-
-    private function getOptions($userinfo)
-    {
-        list($this->decrypt['uid'], $this->decrypt['username'], $this->decrypt['domain']) = explode(',', $userinfo);
-        return $this->decrypt;
-    }
-
-    public function encrypt()
-    {
-        $session = str_replace('-', '', $this->makeSession());
-        $sessionId = $this->genSessionId($session);
-        return Aes::encrypt($sessionId, $this->secret);
-    }
-
-    public function decrypt($session)
-    {
-        $session = str_replace(' ', '+', rawurldecode($session));
-        $str = Aes::decrypt($session, $this->secret);
-        if (!$str) {
-            return false;
+        if (!empty($this->options_data)) {
+            return $this->options_data;
         }
-        $userinfo = substr($str, 32);
-        return $this->getOptions($userinfo);
+
+        list($this->options_data['uid'], $this->options_data['username'], $this->options_data['domain']) = explode(',', $options);
+        return $this->options_data;
     }
 }
